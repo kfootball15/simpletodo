eab819915620d3eff8e7409e4fb0c0c87a7bf608
kfootball15 <fenster.js@gmail.com>
2018-02-06 10:47:45 -0500

    testing...

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 //"DATABASE_URI": "mongodb://localhost:27017/simpletodo",
 module.exports = {
-  "DATABASE_URI": "mongodb://<dbuser>:<dbpassword>@ds125628.mlab.com:25628/heroku_dtz62txz",
+  "DATABASE_URI": "mongodb://localhost:27017/simpletodo",
   "SESSION_SECRET": "wheresHenry",
 };
--- server/env/index.js
+++ server/env/index.js
@@ -2,8 +2,10 @@ var path = require('path');
 var devConfigPath = path.join(__dirname, 'development.js');
 var productionConfigPath = path.join(__dirname, 'production.js');
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
+// 	console.log("shoulve")
     module.exports = require(productionConfigPath);
-} else {
-    module.exports = require(devConfigPath);
-}
+// } else {
+// 	console.log("Should not have", process.env.NODE_ENV)
+//     module.exports = require(devConfigPath);
+// }
